e7ae2202b8785be8bce2606d7d24f1d48af69464
Sarzy <adigwegodswillchinonso@gmail.com>
2021-06-12 22:21:12 +0100

    routes

--- backend/src/routes/routes.hash.ts
+++ backend/src/routes/routes.hash.ts
@@ -1,1 +1,17 @@
- 
+import hashController from '../controllers/controller.hash';
+
+async function routes(app: any, opts: any) {
+  app.route({
+    method: 'GET',
+    url: '/hash/:userId',
+    handler: hashController.getHashBasedOnUser,
+  });
+
+  app.route({
+    method: 'POST',
+    url: '/hash/create',
+    handler: hashController.addHash,
+  });
+}
+
+export default routes;
